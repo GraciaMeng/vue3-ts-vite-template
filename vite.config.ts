2ffef74320e0f4c46bb90f80bcbe2f07ad094256
MengJiaXi <1040616435@qq.com>
2021-11-16 18:56:38 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      views: '@/views',
+      components: '@/components',
+      content: 'components/content',
+      common: 'components/common',
+      assets: '@/assets',
+      api: '@/api',
+      utils: '@/utils',
+      store: '@/store',
+      hooks: '@/hooks',
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        // 自定义
+        target: '', // 这里可以跟随项目实际部署服务器来
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
